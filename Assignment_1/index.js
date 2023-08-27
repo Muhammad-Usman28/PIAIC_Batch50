d3247a6a70ef045bbdddab06ebb7982820f15a9b
Muhammad Usman <140269245+Muhammad-Usman28@users.noreply.github.com>
2023-08-27 09:04:46 +0500

    Add files via upload

---
+++ Assignment_1/index.js
@@ -0,0 +1,39 @@
+"use strict";
+// Print a message simple message Regarding starting of Learning Typescript
+console.log("Hey Muhammad Usman!, I have started Learning Typescript \n");
+// Store your name in a variable and print it.
+var userName = "Muhammad Usman";
+console.log(userName);
+// New Line
+console.log("\n");
+// - Store 10 numbers in different variables.
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var num4 = 40;
+var num5 = 50;
+var num6 = 60;
+var num7 = 70;
+var num8 = 80;
+var num9 = 90;
+var num10 = 100;
+//- Add all of them and print the SUM.
+var Sum;
+Sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("Sum of ten numbers is: ", Sum);
+//Print the Difference (subtraction).
+var Subtraction;
+Subtraction = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log("The Subtraction of Ten numbers is : ", Subtraction);
+//Print the result after multiplying all.
+var Multiplication;
+Multiplication = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("The Multiplication of Ten numbers is : \n", Multiplication);
+//Take two numbers and print the division result.
+var Division;
+Division = num5 / num6;
+console.log("The Division of num 5 and num 6 is :", Division);
+// - Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+var mixResult;
+mixResult = num1 + num2 - num3 * num4 / num5;
+console.log("The Result after Performing Precedence is :\n", mixResult);
---
+++ Assignment_1/index.ts
@@ -0,0 +1,53 @@
+// Print a message simple message Regarding starting of Learning Typescript
+console.log("Hey Muhammad Usman!, I have started Learning Typescript \n") 
+
+// Store your name in a variable and print it.
+
+var userName:string="Muhammad Usman"
+console.log(userName)
+
+// New Line
+console.log("\n")
+
+// - Store 10 numbers in different variables.
+var num1:number=10
+var num2:number=20
+var num3:number=30
+var num4:number=40
+var num5:number=50
+var num6:number=60
+var num7:number=70
+var num8:number=80
+var num9:number=90
+var num10:number=100
+
+//- Add all of them and print the SUM.
+
+var Sum:number
+Sum=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10
+console.log("Sum of ten numbers is: " ,Sum)
+
+//Print the Difference (subtraction).
+
+var Subtraction:number
+Subtraction=num1-num2-num3-num4-num5-num6-num7-num8-num9-num10
+console.log("The Subtraction of Ten numbers is : ",Subtraction)
+
+//Print the result after multiplying all.
+
+var Multiplication:number
+Multiplication=num1*num2*num3*num4*num5*num6*num7*num8*num9*num10
+console.log("The Multiplication of Ten numbers is : ", Multiplication)
+
+
+//Take two numbers and print the division result.
+
+var Division:number
+Division=num5/num6
+console.log("The Division of num 5 and num 6 is :", Division)
+
+// - Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+
+var mixResult:number
+mixResult=num1+num2-num3*num4/num5
+console.log("The Result after Performing Precedence is :",mixResult)
